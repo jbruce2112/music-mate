b89cbb86daeffc738666557646eafa2db6b92e28
John Bruce <jbruce2112@gmail.com>
2017-04-02 10:09:35 -0400

    Initial Commit

---
+++ Song Traits/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Song Traits
+//
+//  Created by John on 4/2/17.
+//  Copyright © 2017 Bruce32. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
